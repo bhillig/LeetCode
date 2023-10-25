a5a4e11272818d218e47abf8ff4f3a8a645f30a7
Brandon Hillig <69443656+bhillig@users.noreply.github.com>
2023-10-24 22:49:06 -0700

    Create 125-ValidPalindrome.cpp

---
+++ 125-ValidPalindrome.cpp
@@ -0,0 +1,33 @@
+// 125 - Valid Palindrome (LeetCode Exercise)
+
+#include <string>
+
+std::string removeSpecialCharacter(std::string s)
+{
+    // Initialize an empty string
+    std::string ans = "";
+    for (auto ch : s) {
+        // if the current character
+        // is an alphabet
+        if (isalnum(ch))
+            ans += ch;
+    }
+    return ans;
+}
+
+bool isPalindrome(std::string b) {
+
+    std::string s = removeSpecialCharacter(b);
+
+    for(size_t i = 0; i < s.size(); ++i)
+    {
+        if (i > (s.size() -1 - i)) break;
+
+        char t = tolower(s[i]);
+        char u = tolower(s[s.size() - 1 - i]);
+
+        if(t != u) return false;
+    }
+
+    return true;
+}
